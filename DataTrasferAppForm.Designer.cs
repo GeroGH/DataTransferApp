aeeb4bad9115d15fb869083e72725bac82f29801
Gergan Gospodinov <gospodinovgergan@gmail.com>
2024-03-04 10:57:46 +0000

    Initial Upload

---
+++ DataTrasferAppForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DataTransferApp
+{
+    partial class DataTransferApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DeleteFields = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.UpdateFields = new System.Windows.Forms.Button();
+            this.OpenExcelFile = new System.Windows.Forms.Button();
+            this.FileLocationLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DeleteFields
+            // 
+            this.DeleteFields.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteFields.ForeColor = System.Drawing.Color.Firebrick;
+            this.DeleteFields.Location = new System.Drawing.Point(381, 12);
+            this.DeleteFields.Name = "DeleteFields";
+            this.DeleteFields.Size = new System.Drawing.Size(363, 58);
+            this.DeleteFields.TabIndex = 7;
+            this.DeleteFields.Text = "Clean Fields Values";
+            this.DeleteFields.UseVisualStyleBackColor = true;
+            this.DeleteFields.Click += new System.EventHandler(this.DeleteFields_Click);
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.Location = new System.Drawing.Point(12, 76);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(1100, 140);
+            this.DataGridView.TabIndex = 6;
+            // 
+            // UpdateFields
+            // 
+            this.UpdateFields.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateFields.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.UpdateFields.Location = new System.Drawing.Point(12, 12);
+            this.UpdateFields.Name = "UpdateFields";
+            this.UpdateFields.Size = new System.Drawing.Size(363, 58);
+            this.UpdateFields.TabIndex = 5;
+            this.UpdateFields.Text = "Update Fields Values";
+            this.UpdateFields.UseVisualStyleBackColor = true;
+            this.UpdateFields.Click += new System.EventHandler(this.UpdateFields_Click);
+            // 
+            // OpenExcelFile
+            // 
+            this.OpenExcelFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenExcelFile.ForeColor = System.Drawing.Color.ForestGreen;
+            this.OpenExcelFile.Location = new System.Drawing.Point(749, 12);
+            this.OpenExcelFile.Name = "OpenExcelFile";
+            this.OpenExcelFile.Size = new System.Drawing.Size(363, 58);
+            this.OpenExcelFile.TabIndex = 9;
+            this.OpenExcelFile.Text = "Open Excel File";
+            this.OpenExcelFile.UseVisualStyleBackColor = true;
+            this.OpenExcelFile.Click += new System.EventHandler(this.OpenExcelFile_Click);
+            // 
+            // FileLocationLabel
+            // 
+            this.FileLocationLabel.AutoSize = true;
+            this.FileLocationLabel.Location = new System.Drawing.Point(12, 222);
+            this.FileLocationLabel.Name = "FileLocationLabel";
+            this.FileLocationLabel.Size = new System.Drawing.Size(90, 13);
+            this.FileLocationLabel.TabIndex = 10;
+            this.FileLocationLabel.Text = "FileLocationLabel";
+            // 
+            // DataTransferApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 242);
+            this.Controls.Add(this.FileLocationLabel);
+            this.Controls.Add(this.OpenExcelFile);
+            this.Controls.Add(this.DeleteFields);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.UpdateFields);
+            this.Name = "DataTransferApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Transfer Application v 2024.03.04a";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.DataTransferApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DeleteFields;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Button UpdateFields;
+        private System.Windows.Forms.Button OpenExcelFile;
+        private System.Windows.Forms.Label FileLocationLabel;
+    }
+}
+
---
+++ DataTrasferAppForm.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using ExcelDataReader;
+using Tekla.Structures.Model;
+using ModelObjectSelector = Tekla.Structures.Model.UI.ModelObjectSelector;
+
+namespace DataTransferApp
+{
+
+    public partial class DataTransferApp : Form
+    {
+        readonly string DataFileLocation = @"C:\Users\gergan.gospodinov\OneDrive - Bourne Group\TeklaStructuresModels_2023\3375-1 South Ruislip Head House\HS2 Asset Data.xlsx";
+        public DataTransferApp()
+        {
+            this.InitializeComponent();
+        }
+
+        private void DataTransferApp_Load(object sender, System.EventArgs e)
+        {
+            using (var stream = File.Open(this.DataFileLocation, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var conf = new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = config => new ExcelDataTableConfiguration
+                        {
+                            UseHeaderRow = true
+                        }
+                    };
+
+                    var dataSet = reader.AsDataSet(conf);
+                    var dataTableCollection = dataSet.Tables;
+                    var AssetClasses = dataTableCollection["Asset Classes"];
+                    this.DataGridView.DataSource = AssetClasses;
+                    this.DataGridView.RowHeadersVisible = false;
+                    this.DataGridView.AllowUserToResizeRows = false;
+                    this.DataGridView.ReadOnly = true;
+                    this.DataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                    this.DataGridView.MultiSelect = false;
+                    this.DataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                    this.DataGridView.AllowUserToAddRows = false;
+                    this.FileLocationLabel.Text = this.DataFileLocation;
+                }
+            }
+        }
+
+        private void UpdateFields_Click(object sender, System.EventArgs e)
+        {
+            var mos = new ModelObjectSelector();
+            var moe = mos.GetSelectedObjects();
+            var partsToSelelect = new ArrayList();
+
+            while (moe.MoveNext())
+            {
+                var part = moe.Current as Part;
+                if (part == null)
+                {
+                    continue;
+                }
+
+                var cells = this.DataGridView.CurrentRow.Cells;
+
+                foreach (DataGridViewCell cell in cells)
+                {
+                    var columnName = this.DataGridView.Columns[cell.ColumnIndex].Name;
+                    var value = cell.Value.ToString();
+
+                    part.SetUserProperty(columnName, value);
+                }
+
+                mos.Select(new ArrayList() { part }, false);
+                partsToSelelect.Add(part);
+            }
+
+            mos.Select(partsToSelelect, false);
+        }
+
+        private void DeleteFields_Click(object sender, System.EventArgs e)
+        {
+            var mos = new ModelObjectSelector();
+            var moe = mos.GetSelectedObjects();
+            var partsToSelelect = new ArrayList();
+
+            while (moe.MoveNext())
+            {
+                var part = moe.Current as Part;
+                if (part == null)
+                {
+                    continue;
+                }
+
+                var columns = this.DataGridView.Columns;
+
+                foreach (DataGridViewColumn column in columns)
+                {
+                    var columnName = column.Name;
+                    part.SetUserProperty(columnName, "");
+                }
+
+                mos.Select(new ArrayList() { part }, false);
+                partsToSelelect.Add(part);
+            }
+
+            mos.Select(partsToSelelect, false);
+        }
+
+        private void OpenExcelFile_Click(object sender, System.EventArgs e)
+        {
+            Process.Start(this.DataFileLocation);
+        }
+    }
+}
